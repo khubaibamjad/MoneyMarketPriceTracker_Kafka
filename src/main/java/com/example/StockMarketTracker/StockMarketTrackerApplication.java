7043512b3b018793203e8dc2728e440dfd1549ce
khubaib213 <khubaib.amjad2002@gmail.com>
2026-09-19 05:53:30 -0700

    Initial Commit

---
+++ src/main/java/com/example/StockMarketTracker/StockMarketTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockMarketTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StockMarketTracker/StockMarketTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StockMarketTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
