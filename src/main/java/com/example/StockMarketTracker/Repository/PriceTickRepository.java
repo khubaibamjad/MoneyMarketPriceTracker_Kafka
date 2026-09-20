5fa270afcb004475360022a8d80e644387019b19
khubaib213 <khubaib.amjad2002@gmail.com>
2026-09-20 09:44:47 -0700

    Created controller for to get All sybols and GetBysymbol

--- src/main/java/com/example/StockMarketTracker/Repository/PriceTickRepository.java
+++ src/main/java/com/example/StockMarketTracker/Repository/PriceTickRepository.java
@@ -3,9 +3,12 @@ package com.example.StockMarketTracker.Repository;
 import com.example.StockMarketTracker.Entity.AssetType;
 import com.example.StockMarketTracker.Entity.PriceTick;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+
+@Component
 public interface PriceTickRepository extends JpaRepository<PriceTick, Long> {
 
    List<PriceTick> findByAssetType(AssetType assetType);
