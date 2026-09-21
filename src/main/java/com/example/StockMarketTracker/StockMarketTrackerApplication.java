25323b2265a8de56c89c4a805aa9c11579624655
khubaib213 <khubaib.amjad2002@gmail.com>
2026-09-21 04:47:08 -0700

    Added a bean for the Kfka topic in the main application ruote

--- src/main/java/com/example/StockMarketTracker/StockMarketTrackerApplication.java
+++ src/main/java/com/example/StockMarketTracker/StockMarketTrackerApplication.java
@@ -1,7 +1,10 @@
 package com.example.StockMarketTracker;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class StockMarketTrackerApplication {
@@ -9,5 +12,15 @@ public class StockMarketTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StockMarketTrackerApplication.class, args);
 	}
+
+
+    @Bean
+    public NewTopic priceTicksTopic()
+    {
+        return TopicBuilder.name("price-ticks")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
 
 }
