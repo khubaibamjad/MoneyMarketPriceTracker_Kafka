8d163ca94ac4c5ae182350149bd00ed3e668e27f
khubaib213 <khubaib.amjad2002@gmail.com>
2026-09-23 18:20:38 -0700

    Forgot to add @EnableScheduling annotation in the main apllication

--- src/main/java/com/example/StockMarketTracker/StockMarketTrackerApplication.java
+++ src/main/java/com/example/StockMarketTracker/StockMarketTrackerApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockMarketTrackerApplication {
 
 	public static void main(String[] args) {
